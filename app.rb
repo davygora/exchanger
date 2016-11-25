3740be01514658fb998fcb8db99aeddc8bbfcc73
davygora <davygora.e@gmail.com>
2016-11-25 02:57:07 +0200

    update main file

---
+++ app.rb
@@ -0,0 +1,58 @@
+require 'open-uri'
+require_relative 'constant'
+require 'csv-mapper'
+require 'sqlite3'
+require 'active_record'
+require_relative 'main_data'
+
+Date::DATE_FORMATS[:default] = '%F'
+Time::DATE_FORMATS[:db] = '%F' # without '00:00:00' in db
+
+#donwload CSV file
+donwload = open(URL)
+File.open('test1.csv', "wb") do |file|
+  file.write open(donwload).read
+end
+
+#create connection
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: ':memory:'
+)
+
+# create table
+ActiveRecord::Schema.define do
+  create_table :main_data, force: true do |t|
+    t.datetime :date
+    t.float    :course
+  end
+end
+
+#import data from CSV to DB
+include CsvMapper
+
+results = import('test1.csv') do
+  map_to MainData
+  after_row lambda{ |row, data| data.save }
+
+  start_at_row 5
+  [date, course]
+end
+
+class Exchanger < MainData
+
+  def self.exchange(count, date)
+    if date.kind_of?(Array)
+      results = []
+      date.each do |d|
+        results << count * rate(d)
+      end
+      results
+    else
+      result = count * rate(date)
+    end
+  end
+end
+
+p Exchanger.exchange(100, [Date.yesterday, Date.today-10.day])
+p Exchanger.exchange(100, '2016-10-26')
